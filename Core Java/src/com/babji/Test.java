6fafcff066dabd51901c2c9863367debae668bbf
narasimhaka <lovnarasimha@gmail.com>
2016-07-08 00:50:46 -0400

    new
    newly

---
+++ Core Java/src/com/babji/Test.java
@@ -0,0 +1,13 @@
+package com.babji;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Narasimharao");
+		System.out.println(10+20);
+	}
+
+}
